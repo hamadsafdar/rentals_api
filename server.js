a6e03f12bc78bf740504bcde6b475e1c984a4d00
Hammad Safdar <hamadsafdar57@gmail.com>
2021-03-13 00:39:04 +0500

    added port enviornment variable

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config({ path: './src/.env' }); //should be called immediately
 require('./src/database'); // will connect database
 
@@ -6,9 +5,8 @@ const http = require('http');
 const app = require('./src');
 
 const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
-
-server.listen(3000, () => {
-	console.log('Rentals server is listening on 3000');
+server.listen(PORT, () => {
+	console.log('Rentals server is listening on ' + PORT);
 });
-
